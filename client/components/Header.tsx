d300de69c3f727157f5e2c2b7a2d5bca979615e2
esteioservice <58089317+esteioservice@users.noreply.github.com>
2021-11-20 20:03:21 -0300

    tradução para portugues

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -62,7 +62,7 @@ const Header: FC = () => {
           forButton
         >
           <Button height={[32, 40]}>
-            {!DISALLOW_REGISTRATION ? "Log in / Sign up" : "Log in"}
+            {!DISALLOW_REGISTRATION ? "Logar / Cadastre-se" : "Log in"}
           </Button>
         </ALink>
       </Link>
@@ -125,20 +125,9 @@ const Header: FC = () => {
             p={0}
           >
             <Li>
-              <ALink
-                href="//github.com/thedevs-network/kutt"
-                target="_blank"
-                rel="noopener noreferrer"
-                title="GitHub"
-                fontSize={[14, 16]}
-              >
-                GitHub
-              </ALink>
-            </Li>
-            <Li>
               <Link href="/report">
-                <ALink href="/report" title="Report abuse" fontSize={[14, 16]}>
-                  Report
+                <ALink href="/report" title="Reportar abuso" fontSize={[14, 16]}>
+                  Reportar
                 </ALink>
               </Link>
             </Li>
@@ -156,7 +145,7 @@ const Header: FC = () => {
           <Flex display={["flex", "none"]}>
             <Link href="/report">
               <ALink href="/report" title="Report" fontSize={[14, 16]}>
-                Report
+                Reportar
               </ALink>
             </Link>
           </Flex>
