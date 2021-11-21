2736c3472fa5d7b68a0786e66d4f80e94d78fa27
esteioservice <58089317+esteioservice@users.noreply.github.com>
2021-11-20 23:50:15 -0300

    tradução para portugues

--- client/pages/terms.tsx
+++ client/pages/terms.tsx
@@ -10,54 +10,54 @@ const TermsPage = () => (
   <AppWrapper>
     {/* TODO: better container */}
     <Col width={600} maxWidth="97%" alignItems="flex-start">
-      <h3>{publicRuntimeConfig.SITE_NAME} Terms of Service</h3>
+      <h3>{publicRuntimeConfig.SITE_NAME} Termos do Serviço</h3>
       <p>
-        By accessing the website at{" "}
+        Acessando o site em{" "}
         <a href={`https://${publicRuntimeConfig.DEFAULT_DOMAIN}`}>
           https://{publicRuntimeConfig.DEFAULT_DOMAIN}
         </a>
-        , you are agreeing to be bound by these terms of service, all applicable
-        laws and regulations, and agree that you are responsible for compliance
-        with any applicable local laws. If you do not agree with any of these
-        terms, you are prohibited from using or accessing this site. The
-        materials contained in this website are protected by applicable
-        copyright and trademark law.
+        , você concorda em obedecer a estes termos de serviço, todos aplicáveis
+        leis e regulamentos e concorda que é responsável pela conformidade
+        com quaisquer leis locais aplicáveis. Se você não concorda com qualquer um destes
+        termos, você está proibido de usar ou acessar este site. o
+        materiais contidos neste site são protegidos por
+        direito autoral e direito de marca registrada.
       </p>
       <p>
-        In no event shall {publicRuntimeConfig.SITE_NAME} or its suppliers be
-        liable for any damages (including, without limitation, damages for loss
-        of data or profit, or due to business interruption) arising out of the
-        use or inability to use the materials on{" "}
-        {publicRuntimeConfig.DEFAULT_DOMAIN} website, even if{" "}
-        {publicRuntimeConfig.SITE_NAME} or a {publicRuntimeConfig.SITE_NAME}{" "}
-        authorized representative has been notified orally or in writing of the
-        possibility of such damage. Because some jurisdictions do not allow
-        limitations on implied warranties, or limitations of liability for
-        consequential or incidental damages, these limitations may not apply to
-        you.
+        Em nenhum caso {publicRuntimeConfig.SITE_NAME} ou seus fornecedores sejam
+        responsável por quaisquer danos (incluindo, sem limitação, danos por perda
+        de dados ou lucro, ou devido à interrupção de negócios) decorrentes do
+        uso ou incapacidade de usar os materiais em{" "}
+        {publicRuntimeConfig.DEFAULT_DOMAIN} website, mesmo que{" "}
+        {publicRuntimeConfig.SITE_NAME} ou um {publicRuntimeConfig.SITE_NAME}{" "}
+        representante autorizado foi notificado oralmente ou por escrito do
+        possibilidade de tal dano. Porque algumas jurisdições não permitem
+        limitações nas garantias implícitas ou limitações de responsabilidade para
+        danos conseqüentes ou incidentais, essas limitações podem não se aplicar a
+        tu.
       </p>
       <p>
-        The materials appearing on {publicRuntimeConfig.SITE_NAME} website could
-        include technical, typographical, or photographic errors.{" "}
-        {publicRuntimeConfig.SITE_NAME} does not warrant that any of the
-        materials on its website are accurate, complete or current.{" "}
-        {publicRuntimeConfig.SITE_NAME} may make changes to the materials
-        contained on its website at any time without notice. However{" "}
-        {publicRuntimeConfig.SITE_NAME} does not make any commitment to update
-        the materials.
+        Os materiais que aparecem em {publicRuntimeConfig.SITE_NAME} website poderia
+        incluem erros técnicos, tipográficos ou fotográficos." "}
+        {publicRuntimeConfig.SITE_NAME} não garante que qualquer um dos
+        os materiais em seu site são precisos, completos ou atuais.{" "}
+        {publicRuntimeConfig.SITE_NAME} pode fazer alterações nos materiais
+        contidos em seu site a qualquer momento sem aviso prévio. Contudo{" "}
+        {publicRuntimeConfig.SITE_NAME} não se compromete a atualizar
+        os materiais.
       </p>
       <p>
-        {publicRuntimeConfig.SITE_NAME} has not reviewed all of the sites linked
-        to its website and is not responsible for the contents of any such
-        linked site. The inclusion of any link does not imply endorsement by{" "}
-        {publicRuntimeConfig.SITE_NAME} of the site. Use of any such linked
-        website is at the {"user's"} own risk.
+        {publicRuntimeConfig.SITE_NAME} não revisou todos os sites vinculados
+        ao seu site e não é responsável pelo conteúdo de tais
+        site vinculado. A inclusão de qualquer link não implica endosso por{" "}
+        {publicRuntimeConfig.SITE_NAME} do site. Uso de qualquer um desses links
+        o site está no {"user's"} o próprio risco.
       </p>
       <p>
-        {publicRuntimeConfig.SITE_NAME} may revise these terms of service for
-        its website at any time without notice. By using this website you are
-        agreeing to be bound by the then current version of these terms of
-        service.
+        {publicRuntimeConfig.SITE_NAME} pode revisar estes termos de serviço para
+        seu site a qualquer momento sem aviso prévio. Ao usar este site você está
+        concordando em se comprometer com a versão então atual destes termos de
+        serviço.
       </p>
     </Col>
   </AppWrapper>
